5b387895194ebb7bca557dd74e571457e9bf00e0
softeyes <jaredko10@gmail.com>
2020-07-21 18:31:16 +0800

    added loaders

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+import {
+    handleSubmit
+} from './js/formHandler.js'
+
+import {
+    checkForName
+} from './js/nameChecker'
+
+console.log(checkForName);
+
+alert('I exist!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
 const path = require("path")
 const webpack = require("webpack")
-module.exports = {}
+
+module.exports = {
+    entry: './src/client/index.js',
+    module: {
+        rules: [{
+                enforce: 'pre',
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                options: {
+                    cache: true
+                }
+            },
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+}
